2d243d5a9f7e8662c927109aaef0c1bc92a22326
Eluin Dasch Hernandez Huerta <eluindaschhernandezhuerta@gmail.com>
2026-08-28 07:59:30 -0600

    Tipos de Datos Primitivos
    Diferentes tipos de datos primitivos, asi como explicacion para entender como y cuando usar cada uno

---
+++ Basic/02-datatypes.js
@@ -0,0 +1,28 @@
+//Tipos de Datos Primitivos
+
+//Cadena de texto (String)
+let name = "Eluin Hernandez"
+let alias = 'Dasch'
+let email = `eluindaschhernandezhuerta@gmail.com`
+
+// Numeros (number)
+let age = 21 //Entero
+let height = 1.76 //Decimal
+
+// Booleanos (boolean)
+let isStudent = true //Verdadero
+let isTeacher = false //Falso
+
+// Undefined
+let undefinedValue //Para variables declaradas pero aun no han sido inicializadas
+console.log(undefinedValue)
+
+//Null
+let nullValue = null
+
+// Symbol
+let mySymbol = Symbol("mysymbol")
+
+//BigInt
+let myBigInt = BigInt(1214641232667967921342477977453412341425478)
+let myBigInt2 = 12315415129311231812873817123141n
