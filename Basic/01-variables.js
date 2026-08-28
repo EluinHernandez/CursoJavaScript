5b620de73b7142668a5cdf1bdae5c97c1b7cf9e6
Eluin Dasch Hernandez Huerta <eluindaschhernandezhuerta@gmail.com>
2026-08-28 07:42:57 -0600

    Declaracion de Variables
    Diferentes declaraciones de variables, en donde vimos la diferencia entre cada uno de estos

---
+++ Basic/01-variables.js
@@ -0,0 +1,21 @@
+//var
+
+var helloWorld = "Hello, JavaScript!"
+console.log(helloWorld)
+
+helloWorld = "Hola de nuevo, JavaScript!"
+console.log(helloWorld)
+
+//let
+
+let helloWorld2 = "Hola, JavaScript!2"
+console.log(helloWorld2)
+
+helloWorld2 = "Hola de nuevo, JavaScript!2"
+console.log(helloWorld2)
+
+//const
+
+//Unico valor desde que es asignado y no puede ser modificable fuera del codigo
+const helloWorld3 = "Hola, JavaScript!3"
+console.log(helloWorld3)
