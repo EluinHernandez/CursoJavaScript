acce2237dd845e165de04d1a0ab1cbc74336488b
Eluin Dasch Hernandez Huerta <eluindaschhernandezhuerta@gmail.com>
2026-08-28 07:33:32 -0600

    Inicio De Un Nuevo Mundo
    Mi primer hola mundo y algunas variables, asi como usos de comentarios

---
+++ Basic/00-helloworld.js
@@ -0,0 +1,20 @@
+//Esto es un  comentario simple
+
+/*
+Esto es
+un comentario
+de varias lineas
+*/
+
+console.log("hello world!")
+console.log('hello world!')
+
+//console.log("hello world!")
+
+console.log(5)
+console.log("5")
+console.log(5 + 2)
+console.log(5 - 2)
+console.log(5 * 2)
+console.log(5 / 2)
+console.log(5 ** 2)
