b199ca0f94521d7693527d5e5c5db59fb3ab714a
Eluin Dasch Hernandez Huerta <eluindaschhernandezhuerta@gmail.com>
2026-08-28 08:26:58 -0600

    Ejercicios de Practica
    Ejercicios en donde se practica lo anteriormente visto, tratamos de ver errores y que podemos y que no podemos hacer

---
+++ Basic/03-beginner-exercises.js
@@ -0,0 +1,96 @@
+//1. Escribe un comentario en una linea
+
+//Comentario de una Linea Ejercicio
+
+//2. Escribe un comentario en varias lineas
+
+/*Comentario
+Varias
+Lineas*/
+
+//3. Declaria variables con valores asociados a todos los datos de tipo primitivos
+
+let name = "Dasch"
+let age = 21
+let isStudent = true
+let undefinedValue
+let nullValue
+let mySymbol3 = Symbol("mysymbol")
+let myBigInt = BigInt(1214641232667967921342477977453412341425478)
+
+// 4. Imprime por consola el valor de todas las variables
+
+console.log(name)
+console.log(age)
+console.log(isStudent)
+console.log(undefinedValue)
+console.log(nullValue)
+console.log(mySymbol3)
+console.log(myBigInt)
+
+// 5. Imprime por consola el tipo de todas las varibles
+console.log(typeof name)
+console.log(typeof age)
+console.log(typeof isStudent)
+console.log(typeof undefinedValue)
+console.log(typeof nullValue)
+console.log(typeof mySymbol3)
+console.log(typeof myBigInt)
+
+// 6. A continuaciuon, modifica los valores de las variables por otros del mismo tipo
+name = "Eluin"
+age = 18
+isStudent = false
+undefinedValue = 5
+nullValue =10
+myBigInt = 123134123141n
+
+console.log(name)
+console.log(age)
+console.log(isStudent)
+console.log(undefinedValue)
+console.log(nullValue)
+console.log(myBigInt)
+
+// 7. A continuacion, modifica los valores de las variables por otros de distinto tipo
+
+name =12
+age = "23"
+isStudent = 53
+undefinedValue = "adad"
+nullValue = "ada"
+//mySymbol = 12141
+myBigInt = "adwawda"
+
+console.log(name)
+console.log(age)
+console.log(isStudent)
+console.log(undefinedValue)
+console.log(nullValue)
+console.log(myBigInt)
+
+// 8. Declara constantes con valores asociados a todos los tipos de datos primitivos
+const name2 = "Dasch"
+const age2 = 21
+const isStudent2 = true
+//const undefinedValue2
+//const nullValue2
+const mySymbol = Symbol("mysymbol")
+const myBigInt2 = BigInt(1214641232667967921342477977453412341425478)
+
+// 9. A continuacion, modifica los valores de las constantes
+//name2 = "Eluin"
+//age2 = 25
+//isStudent2 = false
+//const undefinedValue2
+//const nullValue2
+//mySymbol = Symbol("mysymbol2")
+//myBigInt2 = BigInt(121464178)
+
+console.log(name2)
+console.log(age2)
+console.log(isStudent2)
+console.log(mySymbol)
+console.log(myBigInt2)
+
+// 10. Comenta las lineas que produzcan algun tipo de error
