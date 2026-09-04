5ccc57b4dcfbac3e606c05cce72d77f27e516687
Eluin Dasch Hernandez Huerta <eluindaschhernandezhuerta@gmail.com>
2026-09-04 08:34:50 -0600

    Ejercicios Array, Set y Map

---
+++ Basic/11-structures-exercises.js
@@ -0,0 +1,69 @@
+// 1. Cree un array que almacene cinco animales
+
+let myArray = ["Tiburon","Pezcado","Gato","Perro","Elefante"]
+console.log(myArray)
+
+// 2. Añade dos mas. Uno al principio y otro al final
+
+myArray.push("Rinoceronte")
+myArray.push("Jirafa")
+console.log(myArray)
+
+// 3. Elimina el que se encuentra en tercera posicion
+
+myArray.splice(2,3)
+console.log(myArray)
+
+// 4. Crea un set que almacene cinco libros
+
+let mySet = new Set (["Caperuzita Roja","Los 3 cerditos","Mil Noches","Rodolfo Enrique","La ultima Luna"])
+console.log(mySet)
+
+// 5. Adiciona dos mas. Uno de ellos repetido
+
+mySet.add("Una noche con la Luna")
+mySet.add("Caperuzita Roja")
+console.log(mySet)
+
+// 6. Elimina uno concreto a tu eleccion
+
+mySet.delete("Caperuzita Roja")
+console.log(mySet)
+
+// 7. Crea un mapa que asocie el numero del mes a su nombre
+
+let myMap = new Map()
+
+myMap = new Map([
+    [1,"Enero"],
+    [2,"Febrero"],
+    [3, "Marzo"],
+    [4,"Abril"],
+    [5,"Mayo"],
+    [6,"Junio"],
+    [7,"Julio"],
+    [8,"Agosto"],
+    [9,"Septiembre"],
+    [10,"Octubre"],
+    [11,"Noviembre"],
+    [12,"Diciembre"]
+])
+
+console.log(myMap)
+
+// 8. Comprueba si el mes numero 5 existe en el map e imprime su valor
+
+console.log(myMap.has(5))
+
+// 9. Adiciona al mapa una clave con un array como que almacene los meses de verano
+
+myMap.set("Verano", ["Junio", "Julio", "Agosto"])
+console.log(myMap)
+
+// 10. Crea un Array, transformalo a un Set y almacenalo en un Map
+
+let mesesInvierno = ["Diciembre", "Enero", "Febrero", "Enero"] // incluye repetido
+let setInvierno = new Set(mesesInvierno)
+
+myMap.set("Invierno", setInvierno)
+console.log(myMap)
